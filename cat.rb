1339bad2a1a614910d37dafd3073237e804cc9b3
Caius Durling <dev@caius.name>
2016-01-10 10:34:44 +0000

    Feature-compatible cat(1) implementation

--- cat.rb
+++ cat.rb
@@ -1,3 +1,27 @@
 #!/usr/bin/env ruby
 
-print ARGF.read
+$stdout.sync = true
+$stdout.flush
+
+files = ARGV
+
+# Handle no arguments by reading stdin
+files[0] ||= "-"
+
+exit_code = 0
+
+files.each do |path|
+  if path == "-"
+    $stdout.print($stdin.read)
+    next
+  end
+
+  if File.exist?(path)
+    $stdout.print(File.read(path))
+  else
+    $stderr.puts "cat: #{path}: No such file or directory"
+    exit_code = 1
+  end
+end
+
+exit(exit_code)
